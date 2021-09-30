5becd562cde050cb6a293e4d1adbda4625b01d69
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2021-09-30 18:37:54 +0900

    Fixed

--- env/production.ts
+++ env/production.ts
@@ -1,6 +1,6 @@
 import envType from "./envType"
 
 export const env: envType = {
-  BASE_URL: "https://kondoa9.github.io/test/",
-  ROOT_URL: "/test/",
+  BASE_URL: "https://kondoa9.github.io/DermAnnotation/",
+  ROOT_URL: "/DermAnnotation/",
 }
