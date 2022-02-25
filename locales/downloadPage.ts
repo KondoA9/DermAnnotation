266f4410f84435a9eeaacc9ab33f8703cd66de9c
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-02-25 14:50:47 +0900

    Release v2.2.1

--- locales/downloadPage.ts
+++ locales/downloadPage.ts
@@ -1,22 +1,21 @@
 export default {
   en: {
     downloadPage: {
-      date: "Updated: January 17, 2022",
+      date: "Updated: February 25, 2022",
       noteForMac:
         'For macOS, a warning will be displayed at the first boot after the update. You can start it normally by right-clicking while pressing the control key from Finder and selecting "Open".',
       releaseLabel: "Changes in the latest version",
       bugLabel: "Known bugs",
       releases: [
-        "Added the feature to open image files directly",
-        "Added the feature to open a file or folder by dragging and dropping on the startup screen",
-        "Changed the icon",
-        "Change the design of the startup screen",
-        "Other few fixes",
+        "Fixed an issue that could cause a crash when saving after adding / removing pens",
+        "Fixed an issue where pen settings might not be saved properly",
+        "Fixed the problem that the layout of the editor is broken when the operation panel is placed on the left side.",
+        "Added tooltips to some UI",
+        "Improved editor operability",
+        "Improve performance",
+        "Added Windows standalone version",
       ],
       bugs: [
-        "May crash when saving after adding / removing pens",
-        "Pen settings may not be saved properly",
-        "The layout of the editor is broken when the operation panel is placed on the left side",
         "Notifications may be interrupted and invisible",
         "Increasing the window size on macOS makes the operation extremely heavy (reducing the window size improves the operation)",
       ],
@@ -24,22 +23,21 @@ export default {
   },
   ja: {
     downloadPage: {
-      date: "最終更新: 2021/01/17",
+      date: "最終更新: 2022/02/25",
       noteForMac:
         "macOSの場合、アップデート後の初回起動時に警告が表示されます。Finderからcontrolキーを押下しながら右クリックし、「開く」を選択することで正常に起動できます。",
       releaseLabel: "最新バージョンの変更点",
       bugLabel: "既知の不具合",
       releases: [
-        "画像ファイルを直接開く機能を追加",
-        "起動画面においてドラッグ＆ドロップでファイルまたはフォルダを開く機能を追加",
-        "アイコンを変更",
-        "起動画面のデザインを変更",
-        "その他軽微な修正等",
+        "ペンを追加 / 削除後に保存するとクラッシュする場合がある問題を修正",
+        "ペン設定の保存が正常に行われない場合がある問題を修正",
+        "操作パネルを左側に配置した際にエディタのレイアウトが崩れる問題を修正",
+        "いくつかのUIにツールチップを追加",
+        "エディタの操作性を改善",
+        "パフォーマンスを向上",
+        "Windowsスタンドアローン版を追加",
       ],
       bugs: [
-        "ペンを追加 / 削除後に保存するとクラッシュする場合がある",
-        "ペン設定の保存が正常に行われない場合がある",
-        "操作パネルを左側に配置した際にエディタのレイアウトが崩れる",
         "通知が途中で途切れて見えない場合がある",
         "macOSでウィンドウサイズを大きくすると動作が極端に重くなる (ウィンドウサイズを小さくすることで改善されます)",
       ],
--- pages/download/index.vue
+++ pages/download/index.vue
@@ -1,45 +1,68 @@
 <template>
   <PageTemplate>
     <div class="container-fluid py-5 text-center bg-light my-auto" style="min-height: 90vh">
-      <h2 class="display-5 pt-3">Download DermAnnotation v2.2.0</h2>
+      <h2 class="display-5 pt-3">Download DermAnnotation v2.2.1</h2>
       <p class="my-3">{{ $t("downloadPage.date") }}</p>
-      <div class="container" style="display: table">
-        <div class="align-middle" style="display: table-cell">
-          <div>
-            <button class="btn btn-primary py-3 px-5 m-3" @click="download('windows')">
-              <p class="lead m-0">Windows (64bit)</p>
-              <p class="m-0">10 ver1803+ / 11</p>
-            </button>
-            <button class="btn btn-primary py-3 px-5 m-3" @click="download('macOS')">
-              <p class="lead m-0">macOS</p>
-              <p class="m-0">Mojave 10.14+</p>
-            </button>
-            <button class="btn btn-primary py-3 px-5 m-3" disabled>
-              <p class="lead m-0">[WIP] Linux</p>
-              <p class="m-0">Ubuntu 20.04 LTS</p>
-            </button>
+
+      <div class="container mt-4">
+        <b-card no-body bg-variant="light">
+          <b-tabs align="center" pills card>
+            <b-tab title="Windows" active>
+              <div>
+                <p class="lead mb-2">Windows 10 / 11 64bit</p>
+                <div class="d-flex justify-content-center flex-wrap">
+                  <button class="btn btn-outline-primary px-5 py-2 m-3" @click="download('windows.installer')">
+                    <p>Installer (.msi)</p>
+                    <small>Recommended</small>
+                  </button>
+                  <button class="btn btn-outline-primary px-5 py-2 m-3" @click="download('windows.standalone')">
+                    Standalone (.zip)
+                  </button>
+                </div>
+              </div>
+            </b-tab>
+            <b-tab title="macOS">
+              <div>
+                <p class="lead mb-2">macOS Mojave 10.14+</p>
+                <div class="d-flex justify-content-center flex-wrap">
+                  <button class="btn btn-outline-primary px-5 py-2 m-3" @click="download('macOS')">
+                    <p>Standalone (.zip)</p>
+                  </button>
+                </div>
+              </div>
+            </b-tab>
+            <b-tab title="[WIP] Linux" disabled>
+              <div>
+                <p class="lead mb-2">Ubuntu 20.04 LTS</p>
+                <div class="d-flex justify-content-center flex-wrap">
+                  <button class="btn btn-outline-primary px-5 py-2 m-3" @click="download('linux')">
+                    <p>Installer</p>
+                  </button>
+                </div>
+              </div>
+            </b-tab>
+          </b-tabs>
+        </b-card>
+
+        <p class="my-5">*{{ $t("downloadPage.noteForMac") }}</p>
+
+        <div class="container" style="text-align: left">
+          <div v-show="$t('downloadPage.releases').length !== 0">
+            <h5 class="pt-4">{{ $t("downloadPage.releaseLabel") }}</h5>
+            <ul class="py-2">
+              <li v-for="release in $t('downloadPage.releases')" :key="release">
+                {{ release }}
+              </li>
+            </ul>
           </div>
 
-          <p class="my-3">*{{ $t("downloadPage.noteForMac") }}</p>
-
-          <div class="container pt-5 pb-3" style="text-align: left">
-            <div v-show="$t('downloadPage.releases').length !== 0">
-              <h5 class="pt-4">{{ $t("downloadPage.releaseLabel") }}</h5>
-              <ul class="py-2">
-                <li v-for="release in $t('downloadPage.releases')" :key="release">
-                  {{ release }}
-                </li>
-              </ul>
-            </div>
-
-            <div v-show="$t('downloadPage.bugs').length !== 0">
-              <h5 class="pt-4">{{ $t("downloadPage.bugLabel") }}</h5>
-              <ul class="py-2">
-                <li v-for="bug in $t('downloadPage.bugs')" :key="bug">
-                  {{ bug }}
-                </li>
-              </ul>
-            </div>
+          <div v-show="$t('downloadPage.bugs').length !== 0">
+            <h5 class="pt-4">{{ $t("downloadPage.bugLabel") }}</h5>
+            <ul class="py-2">
+              <li v-for="bug in $t('downloadPage.bugs')" :key="bug">
+                {{ bug }}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -54,13 +77,27 @@ import { seoMixin } from "@/modules/mixins/seoMixin"
 export default Vue.extend({
   mixins: [seoMixin("download")],
   methods: {
-    download(platform: "windows" | "macOS") {
-      const installer = platform === "windows" ? "DermAnnotationSetup_Windows.msi" : "DermAnnotationSetup_macOS.zip"
-      window.location.href = `${this.$router.options.base}publish/installer/${installer}`
-      ;(this as any).$gtag("event", "download", {
-        event_category: "installer",
-        event_label: platform,
-      })
+    download(platform: "windows.installer" | "windows.standalone" | "macOS" | "linux") {
+      const installer = (() => {
+        switch (platform) {
+          case "windows.installer":
+            return "DermAnnotationSetup_Windows.msi"
+          case "windows.standalone":
+            return "DermAnnotationSetup_Windows.zip"
+          case "macOS":
+            return "DermAnnotationSetup_macOS.zip"
+          default:
+            return undefined
+        }
+      })()
+
+      if (installer) {
+        window.location.href = `${this.$router.options.base}publish/installer/${installer}`
+        ;(this as any).$gtag("event", "download", {
+          event_category: "installer",
+          event_label: platform,
+        })
+      }
     },
   },
 })
