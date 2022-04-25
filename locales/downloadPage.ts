e3a35b33a9b7fc8121673e4bf0e78344d127ce29
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-04-25 16:02:30 +0900

    Release v2.2.2

--- locales/downloadPage.ts
+++ locales/downloadPage.ts
@@ -1,24 +1,18 @@
 export default {
   en: {
     downloadPage: {
-      date: "Updated: February 25, 2022",
+      date: "Updated: April 25, 2022",
       noteForMac:
         'For macOS, a warning will be displayed at the first boot after the update. You can start it normally by right-clicking while pressing the control key from Finder and selecting "Open".',
       noteForLinux: "Please contact us if you would like to use the Linux version.",
       releaseLabel: "Changes in the latest version",
       bugLabel: "Known bugs",
       releases: [
-        "Fixed an issue that could cause a crash when saving after adding / removing pens",
-        "Fixed an issue where pen settings might not be saved properly",
-        "Fixed the problem that the layout of the editor is broken when the operation panel is placed on the left side.",
-        "Added tooltips to some UI",
-        "Improved editor operability",
-        "Improve performance",
-        "Added Windows standalone version",
+        "Fixed an issue where the area would shift slightly when performing Back / Forward after filling a range",
+        "Fixed an issue where some pens became inoperable when there were many pens on the pen edit screen (when there are many pens, you can edit all pens by scrolling)",
+        'Fixed an issue where the "Default save destination" setting might not work properly',
       ],
       bugs: [
-        "On the pen editing screen, some pens become inoperable when there are many pens.",
-        '"Default save destination" setting may not work properly',
         "Notifications may be interrupted and invisible",
         "Increasing the window size on macOS makes the operation extremely heavy (reducing the window size improves the operation)",
       ],
@@ -26,24 +20,18 @@ export default {
   },
   ja: {
     downloadPage: {
-      date: "最終更新: 2022/02/25",
+      date: "最終更新: 2022/04/25",
       noteForMac:
         "macOSの場合、アップデート後の初回起動時に警告が表示されます。Finderからcontrolキーを押下しながら右クリックし、「開く」を選択することで正常に起動できます。",
       noteForLinux: "Linux版をご希望の場合はお問い合わせください。",
       releaseLabel: "最新バージョンの変更点",
       bugLabel: "既知の不具合",
       releases: [
-        "ペンを追加 / 削除後に保存するとクラッシュする場合がある問題を修正",
-        "ペン設定の保存が正常に行われない場合がある問題を修正",
-        "操作パネルを左側に配置した際にエディタのレイアウトが崩れる問題を修正",
-        "いくつかのUIにツールチップを追加",
-        "エディタの操作性を改善",
-        "パフォーマンスを向上",
-        "Windowsスタンドアローン版を追加",
+        "範囲塗りつぶし後に戻る / 進むを実行すると領域がわずかにずれる問題を修正",
+        "ペン編集画面において、ペンが多数ある場合に一部のペンが操作不可能な状態になる問題を修正（ペンが多数ある場合はスクロールすることですべてのペンを編集可能です）",
+        "「規定の保存先」設定が正常に働かない場合がある問題を修正",
       ],
       bugs: [
-        "ペン編集画面において、ペンが多数ある場合に一部のペンが操作不可能な状態になる",
-        "「規定の保存先」設定が正常に働かない場合がある",
         "通知が途中で途切れて見えない場合がある",
         "macOSでウィンドウサイズを大きくすると動作が極端に重くなる (ウィンドウサイズを小さくすることで改善されます)",
       ],
