e5a4dc3b8673ed90221e2a5f51af73423378100b
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-04-25 16:58:16 +0900

    update footer

--- components/CustomFooter.vue
+++ components/CustomFooter.vue
@@ -2,7 +2,7 @@
   <footer class="py-4 bg-dark text-light text-center">
     <div class="container">
       <p class="m-0">
-        © 2021
+        © 2021-2022
         <a class="text-light" href="https://github.com/KondoA9">Ekyu Kondo</a>
       </p>
     </div>
