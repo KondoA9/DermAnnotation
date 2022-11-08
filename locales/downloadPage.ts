6d672c5b6d2ca2d4aed6390c8ce8c33b9a2713a1
Kondo Ekyu <58505068+KondoA9@users.noreply.github.com>
2022-11-08 10:33:03 +0900

    release v2.3.5

--- locales/downloadPage.ts
+++ locales/downloadPage.ts
@@ -1,29 +1,25 @@
 export default {
   en: {
     downloadPage: {
-      date: "Updated: September 28, 2022",
+      date: "Updated: November 8, 2022",
       noteForMac:
         'For macOS, a warning will be displayed at the first boot after the update. You can start it normally by right-clicking while pressing the control key from Finder and selecting "Open".',
       noteForLinux: "Please contact us if you would like to use the Linux version.",
       releaseLabel: "Latest version update information",
       bugLabel: "Known bugs",
-      releases: [
-        "[Improvement]: Improved so that if the left mouse button is kept pressed even after going out of the editor area during editing, painting will continue when the area is re-entered.",
-      ],
+      releases: ["[Bug fix]: Fixed issue where cursor style and eraser state were not synced"],
       bugs: [],
     },
   },
   ja: {
     downloadPage: {
-      date: "最終更新: 2022/09/28",
+      date: "最終更新: 2022/11/08",
       noteForMac:
         "macOSの場合、アップデート後の初回起動時に警告が表示されます。Finderからcontrolキーを押下しながら右クリックし、「開く」を選択することで正常に起動できます。",
       noteForLinux: "Linux版をご希望の場合はお問い合わせください。",
       releaseLabel: "最新バージョンの更新情報",
       bugLabel: "既知の不具合",
-      releases: [
-        "[Improvement]: 編集中、エディタ領域外に出た際もマウス左ボタンを押下し続けていた場合、領域内に再度入ったときに塗りを続行するように改善",
-      ],
+      releases: ["[Bug fix]: カーソルスタイルと消しゴムの状態が同期されない問題を修正"],
       bugs: [],
     },
   },
