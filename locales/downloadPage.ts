88e98a10ff0f376ce507dc4febaeaaa156bedbf9
近藤 恵休 <kondo.eque@gmail.com>
2022-11-16 21:07:00 +0900

    release: bump version to 2.3.6

--- locales/downloadPage.ts
+++ locales/downloadPage.ts
@@ -1,25 +1,25 @@
 export default {
   en: {
     downloadPage: {
-      date: "Updated: November 8, 2022",
+      date: "Updated: November 16, 2022",
       noteForMac:
         'For macOS, a warning will be displayed at the first boot after the update. You can start it normally by right-clicking while pressing the control key from Finder and selecting "Open".',
       noteForLinux: "Please contact us if you would like to use the Linux version.",
       releaseLabel: "Latest version update information",
       bugLabel: "Known bugs",
-      releases: ["[Bug fix]: Fixed issue where cursor style and eraser state were not synced"],
+      releases: ["[Bug fix]: Fixed a crash when trying to edit pens"],
       bugs: [],
     },
   },
   ja: {
     downloadPage: {
-      date: "最終更新: 2022/11/08",
+      date: "最終更新: 2022/11/16",
       noteForMac:
         "macOSの場合、アップデート後の初回起動時に警告が表示されます。Finderからcontrolキーを押下しながら右クリックし、「開く」を選択することで正常に起動できます。",
       noteForLinux: "Linux版をご希望の場合はお問い合わせください。",
       releaseLabel: "最新バージョンの更新情報",
       bugLabel: "既知の不具合",
-      releases: ["[Bug fix]: カーソルスタイルと消しゴムの状態が同期されない問題を修正"],
+      releases: ["[Bug fix]: ペンを編集しようとするとクラッシュする問題を修正"],
       bugs: [],
     },
   },
