660e225cac10c2c512cb3a8f205d6aa5507d67f0
KondoA9 <kondo.eque@gmail.com>
2024-03-22 07:15:04 +0000

    Deploying to gh-pages from @ KondoA9/DermAnnotation@6e0e622aad369654f837fda38da1e63a73b97539 🚀

--- _nuxt/static/1700471693/download/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FDermAnnotation\u002F_nuxt\u002Fstatic\u002F1700471693",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdownload\u002F",config:{_app:{basePath:"\u002FDermAnnotation\u002F",assetsPath:"\u002FDermAnnotation\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1711091661/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fcontact","\u002Fdownload","\u002Fdev","\u002Fen\u002Fmanual","\u002Fen\u002Fdownload","\u002Fcontact","\u002Fmanual","\u002Fen\u002Fdev","\u002Fen"]})
